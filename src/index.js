02c2fbf806ac81ecb99bae3150b625a9bd788c46
Derek Tran <tran.derek.d.@gmail.com>
2024-04-13 09:41:00 -0700

    Add and style Navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css';
+
+const menuButton = document.querySelector('.menu-toggle-button');
+const menuLinks = document.querySelector('.menu-links');
+
+menuButton.addEventListener('click', () => {
+  menuButton.classList.toggle('expand');
+  if (menuLinks.classList.contains('expand')) {
+    menuLinks.classList.remove('expand');
+
+    menuLinks.addEventListener('animationend', () => {
+      menuLinks.classList.remove('collapse');
+    }, { once: true });
+    
+    menuLinks.classList.add('collapse')
+  } else {
+    menuLinks.classList.add('expand');
+  }
+});
