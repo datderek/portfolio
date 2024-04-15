f0b382abf68503d09dba8c5e4ef889169cc9a77f
Derek Tran <tran.derek.d@gmail.com>
2024-04-15 16:36:31 -0700

    Add links to projects

--- src/modules/Display.js
+++ src/modules/Display.js
@@ -16,26 +16,31 @@ export default class Display {
     {
       'name': 'Yum',
       'description': 'A social culinary platform for home cooks and professional chefs',
+      'link': 'https://github.com/jackfales/yum',
       'img': createImageElement(Yum)
     },
     {
       'name': 'Battleships',
       'description': 'A single player recreation of the classic Battleship game',
+      'link': 'https://github.com/datderek/battleship.git',
       'img': createImageElement(Battleship)
     },
     {
       'name': 'SoundScribe',
       'description': 'A Discord bot that transcribes audio in real-time',
+      'link': 'https://github.com/datderek/SoundScribe.git',
       'img': createImageElement(SoundScribe)
     },
     {
       'name': 'WeatherMan',
       'description': 'Another one of those Weather apps... but made by me',
+      'link': 'https://github.com/datderek/weatherman.git',
       'img': createImageElement(WeatherMan)
     },
     {
       'name': 'Tic Tac Toe',
       'description': 'Get three in a row!',
+      'link': 'https://github.com/datderek/odinproject/tree/main/tictactoe',
       'img': createImageElement(TicTacToe)
     },
   ]
@@ -45,6 +50,9 @@ export default class Display {
       const projectContainer = document.createElement('li');
       projectContainer.classList.add('project');
       
+      const projectLinkWrapper = document.createElement('a');
+      projectLinkWrapper.href = project.link;
+
       const projectImage = project.img;
       projectImage.classList.add('project-image');
      
@@ -62,6 +70,7 @@ export default class Display {
       projectDetails.appendChild(projectName);
       projectDetails.appendChild(projectDescription);
       projectContainer.appendChild(projectImage);
+      projectContainer.appendChild(projectLinkWrapper);
       projectContainer.appendChild(projectDetails);
       this.projectList.appendChild(projectContainer);
     })
